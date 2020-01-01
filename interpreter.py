74d5628a434edebe1a66522e58757e6f6e1d045b
Dmitry Orekhov <dmitry_orekhov@epam.com>
2020-01-01 10:49:52 +0300

    The first commit

---
+++ interpreter.py
@@ -0,0 +1,29 @@
+text = "A>B\nB|>C"
+
+def interpret(in_str, program, debug_mode = False):
+    log = [(in_str)]
+    while True:
+        stop = False
+        for instruction in program:
+            if log[-1].find(instruction[0]) == -1:
+                continue
+            else:
+                log.append(log[-1].replace(instruction[0], instruction[1], 1))
+                stop = instruction[2]
+                break
+        if stop == True:
+            break
+    
+    if debug_mode == True:
+        return log
+    else:
+        return log[-1]
+
+def parse_line(line):
+    if line.find("|>") != -1:
+            return (str(line).split("|>", 1)[0], str(line).split("|>", 1)[1], True)
+    else:
+        if line.find(">") != -1:
+            return (str(line).split(">", 1)[0], str(line).split(">", 1)[1], False)
+        else:
+            return () 
---
+++ main.py
@@ -0,0 +1,53 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START gae_python37_render_template]
+import datetime
+
+from flask import Flask, render_template, request, jsonify 
+from interpreter import interpret, parse_line
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    #return render_template('index.html', times=dummy_times)
+    return render_template('index.html')
+
+@app.route('/start-program', methods = ['POST'])
+def jsonexample():
+
+    if request.method == 'POST':
+        lines = request.form['message'].splitlines()
+        
+        program = []
+        for l in lines:
+            program.append(parse_line(l))
+            
+        return jsonify(interpret("AAAA", program, True))
+    else: 
+        return "Oops"
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host='127.0.0.1', port=5000, debug=True)
+# [START gae_python37_render_template]
